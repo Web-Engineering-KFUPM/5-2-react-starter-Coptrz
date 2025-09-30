d7b42c451aa5723dcb1d42a997e8bbbac64f3c0e
Coptrz <nwaclos@gmail.com>
2025-09-30 12:08:48 +0300

    almost finished the first task , will complete it later

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard(props){
+
+    return (
+        <div>
+
+            <h3>Abdulwahab Badirah{props.name}</h3>
+            <p>202160410</p>
+            <p>ICS</p>
+            </div>);
+            }
+            export default StudentCard;
